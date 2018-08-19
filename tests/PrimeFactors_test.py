f386bb3196290ad0479bf7f30eba720ec7107077
Pin-Hsien Lee <pspeter9931@gmail.com>
2018-08-19 11:35:09 +0800

    Group the first 9 tests to one single test
    Move tests 1 to 9 to 'testFromOneToNine'.

--- tests/PrimeFactors_test.py
+++ tests/PrimeFactors_test.py
@@ -6,39 +6,28 @@ def newList(*args):
 
 class TestPrimeFactors(unittest.TestCase):
 
-    def testOne(self):
-        self.assertEquals(newList(), PrimeFactors.generate(1))
-
-    def testTwo(self):
-        self.assertEquals(newList(2), PrimeFactors.generate(2))
-
-    def testThree(self):
-        self.assertEquals(newList(3), PrimeFactors.generate(3))
-
-    def testFour(self):
-        self.assertEquals(newList(2, 2), PrimeFactors.generate(4))
-
-    def testFive(self):
-        self.assertEquals(newList(5), PrimeFactors.generate(5))
-
-    def testSix(self):
-        self.assertEquals(newList(2, 3), PrimeFactors.generate(6))
-
-    def testSeven(self):
-        self.assertEquals(newList(7), PrimeFactors.generate(7))
-
-    def testEight(self):
-        self.assertEquals(newList(2, 2, 2), PrimeFactors.generate(8))
-
-    def testNine(self):
-        self.assertEquals(newList(3, 3), PrimeFactors.generate(9))
-
     def testZero(self):
         self.assertRaises(ValueError, PrimeFactors.generate, 0)
 
     def testNegativeValue(self):
         self.assertRaises(ValueError, PrimeFactors.generate, -1)
 
+    def testFromOneToNine(self):
+        from_one_to_nine = [
+            (1, []),
+            (2, [2]),
+            (3, [3]),
+            (4, [2, 2]),
+            (5, [5]),
+            (6, [2, 3]),
+            (7, [7]),
+            (8, [2, 2, 2]),
+            (9, [3, 3])
+        ]
+        for num, primes in from_one_to_nine:
+            self.assertEqual(
+                newList(*primes), PrimeFactors.generate(num),
+                msg=f'Failed on {num}')
 
 
 if __name__ == '__main__':
